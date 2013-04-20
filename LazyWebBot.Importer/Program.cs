2632e51f6fe161b1c21bee850cc609ea68a861d5
tombuildsstuff <git@ibuildstuff.co.uk>
2013-04-20 11:28:48 +0100

    Added in search from twitter

---
+++ LazyWebBot.Importer/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace LazyWebBot.Importer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ LazyWebBot.Playground/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace LazyWebBot.Playground
+{
+    using System;
+    using System.Configuration;
+
+    using TweetSharp;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var consumerKey = ConfigurationManager.AppSettings["consumerKey"];
+            var consumerSecret = ConfigurationManager.AppSettings["consumerSecret"];
+            var userAccessToken = ConfigurationManager.AppSettings["userAccessToken"];
+            var userTokenSecret = ConfigurationManager.AppSettings["userTokenSecret"];
+            var searchTerm = ConfigurationManager.AppSettings["searchTerm"];
+
+            var service = new TwitterService(consumerKey, consumerSecret);
+            service.AuthenticateWith(userAccessToken, userTokenSecret);
+            VerifyUser(service);
+            
+            RunSearch(service, searchTerm);
+        }
+
+        private static void RunSearch(ITwitterService service, string searchTerm)
+        {
+            var results = service.Search(new SearchOptions { Q = searchTerm, Lang = "en", Resulttype = TwitterSearchResultType.Mixed });
+            foreach (var result in results.Statuses)
+            {
+                Console.WriteLine(result.Text);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void VerifyUser(ITwitterService service)
+        {
+            var user = service.VerifyCredentials(new VerifyCredentialsOptions());
+            Console.WriteLine("{0} ({1})", user.Name, user.ScreenName);
+        }
+    }
+}
