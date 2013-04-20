f87f97445753e3f0dba241d7301b9a2a95ffdbcf
tombuildsstuff <git@ibuildstuff.co.uk>
2013-04-20 15:12:26 +0100

    Added in an empty / basic replier handler

---
+++ LazyWebBot.Contracts/Recommendation.cs
@@ -0,0 +1,15 @@
+﻿namespace LazyWebBot.Contracts
+{
+    using System;
+
+    public class Recommendation : IEntity
+    {
+        public Guid Id { get; set; }
+
+        public Status OriginalTweet { get; set; }
+
+        public string RecommendationUrl { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
---
+++ LazyWebBot.Replier/RecommendationHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace LazyWebBot.Replier
+{
+    using LazyWebBot.Contracts;
+
+    using NServiceBus;
+
+    public class RecommendationHandler : IHandleMessages<Recommendation>
+    {
+        public void Handle(Recommendation message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
