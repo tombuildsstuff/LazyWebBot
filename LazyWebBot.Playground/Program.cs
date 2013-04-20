5518c76e9cf96c5a2d63e935a4ce12396261e0ad
tombuildsstuff <git@ibuildstuff.co.uk>
2013-04-20 16:21:47 +0100

    Changed the playground app so that it hits google for the search service

--- LazyWebBot.Playground/Program.cs
+++ LazyWebBot.Playground/Program.cs
@@ -1,9 +1,28 @@
 ﻿namespace LazyWebBot.Playground
 {
+    using System;
+
+    using LazyWebBot.Services.Search;
+
     public class Program
     {
         public static void Main(string[] args)
         {
+            var service = new SearchService();
+            var terms = new[]
+            {
+                "Parker Pen refills",
+                "online invoice generators",
+                "batch-convert .m4a-&gt;.mp3, regardless of folder structure",
+            };
+            
+            foreach (var term in terms)
+            {
+                var uri = service.GetUriForTerm(term);
+                Console.WriteLine(uri != null ? uri.ToString() : "(not found)");
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- LazyWebBot.Services/Search/SearchService.cs
+++ LazyWebBot.Services/Search/SearchService.cs
@@ -9,22 +9,16 @@
         {
             var encodedTerm = Uri.EscapeDataString(term);
             var url = string.Format("http://www.google.co.uk/search?output=search&sclient=psy-ab&q={0}&btnI=", encodedTerm);
-            var request = WebRequest.Create(url);
-            {
-                request.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0");
-                request.Headers.Add("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-                request.Headers.Add("accept-language", "en-US,en;q=0.5");
-                request.Headers.Add("accept-encoding", "gzip, deflate");
-                request.Headers.Add("referrer", "http://www.google.co.uk");
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0";
+            request.Headers.Add("accept-language", "en-US,en;q=0.5");
+            request.Headers.Add("accept-encoding", "gzip, deflate");
+            request.Headers.Add("referrer", "http://www.google.co.uk");
 
-                var response = (HttpWebResponse)request.GetResponse();
-                if (response.StatusCode == HttpStatusCode.Moved)
-                {
-                    return request.RequestUri;
-                }
-            }
+            var response = (HttpWebResponse)request.GetResponse();
 
-            return null;
+            return response.StatusCode == HttpStatusCode.OK ? response.ResponseUri : null;
         }
     }
 }
